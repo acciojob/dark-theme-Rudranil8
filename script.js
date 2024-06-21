573393a6b0179b9fc79f917476f3f573b30f0394
Rudranil8 <95670098+Rudranil8@users.noreply.github.com>
2024-06-21 19:00:08 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,2 +1,9 @@
 let x=document.querySelector("#ptag");
 x.style.color="blue";
+let appDiv = document.getElementById("app");
+let swapButton = document.getElementById("swap");
+function swapTheme(){
+	appDiv.classList.toggle("night");
+	swapButton.classList.toggle("button_night");
+}
+swapButton.onclick = swapTheme;
